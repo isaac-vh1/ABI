9bf2f62ef56859e615c10d98fccd0c6f71e30c54
Isaac (PI) <isaac@vanhorn.com>
2025-03-04 08:29:53 +0000

    Adding Client page

---
+++ clients/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import InvoicePage from "./InvoicePage.js"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <main>
+        <InvoicePage />
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
